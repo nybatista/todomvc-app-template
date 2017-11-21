8cc9c9b2dd78d4b90fbe69ef027cca5ce4535180
Frank Batista <nybatista@aol.com>
2017-11-21 04:26:23 -0500

    Spyne added as global

--- js/app.js
+++ js/app.js
@@ -2,5 +2,12 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
+	console.log(window.document.readyState);
+
+	let fn = (e) => {
+			console.log("window state is ",e.target.readyState,R,Rx,spyne);
+	};
+
+	window.document.addEventListener('DOMContentLoaded', fn);
 
 })(window);
