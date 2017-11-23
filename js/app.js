0785a0f1ca19fea43760688d5c4358aa1312e014
Frank Batista <nybatista@aol.com>
2017-11-22 22:06:19 -0500

    spyne template is working

--- js/app.js
+++ js/app.js
@@ -13,11 +13,27 @@
 		val: 'tot'
 	};
 
-	let tmpl = document.querySelector('#test-li').text;
-	console.log("tmpl data ",tmpl,data);
 
-	let myView = new Spyne.DomItem('h1', data,tmpl);
+	let tmpl = document.querySelector('#test-li').text;
+
+	var re =/(\{\{)([\w]+)(\}\})/gm;
+
+	var getStr = (str,p1,p2,p3,p4) => {
+		let newStr = data[p2];
+		console.log('newString ',newStr,p2);
+		return newStr;
+	}
+
+	let str = tmpl.replace(re, getStr);
+	let el =  new DOMParser().parseFromString(str, 'text/html').body.childNodes[0];
+
+	var todosTmpl = document.querySelector('#test-li');
+	console.log("tmpl data ",el,str);
+
+	let myView = new Spyne.DomItem('div', {}, data, todosTmpl);
+	//document.body.appendChild(el);
 	document.body.appendChild(myView.render());
+	console.log("render ",myView.render());
 
 	window.document.addEventListener('DOMContentLoaded', fn);
 
