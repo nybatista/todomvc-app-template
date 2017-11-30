6278494af404c8bd1f1804fc616d885f7fe648f9
Frank Batista <nybatista@aol.com>
2017-11-30 00:54:44 -0500

    cleaner

--- js/model.js
+++ js/model.js
@@ -1,32 +1,24 @@
  class TodosModel extends spyne.ChannelsBase {
 	constructor(){
-		NodeList.prototype.map = HTMLCollection.prototype.map = Array.prototype.map;
 
 		super();
 		this.settings.name = 'MODEL';
 
 		this.STORAGE_KEY = 'todos-yaya';
 
-		this.actions = {
+/*		this.actions = {
 			INIT_TODOS_EVENT   :  'INIT_TODOS_EVENT',
 			ADD_TODO_EVENT     :  'ADD_TODO_EVENT',
 			REMOVE_TODO_EVENT  :  'REMOVE_TODO_EVENT',
 			UPDATE_TODOS_EVENT :  'UPDATE_TODOS_EVENT'
-		};
-
-		const paramActions = this.paramActions = {
-			completed: 'UPDATE_TODOS_EVENT',
-			destroy: 'REMOVE_TODO_EVENT',
-			title: 'UPDATE_TODOS_EVENT',
-			titleNew: 'ADD_TODO_EVENT'
+		};*/
 
 
-		};
 
 		this.localStorageObj = this.getStorageItems();
-		window.localObj = this.localStorageObj;
+/*		window.localObj = this.localStorageObj;
 		this.idIter = this.getHighestIdNum();
-		console.log('id iter  = ',this.idIter);
+		console.log('id iter  = ',this.idIter);*/
 
 		this.observer$ = new Rx.BehaviorSubject(
 			{
@@ -34,78 +26,8 @@
 				payload: this.localStorageObj
 			});
 
-
-
-
-		// BASE METHODS
-/*
-
-		const updateParamsInObj = (params) => {
-			console.log("params in upate ",params);
-			const itemInArr = R.propSatisfies(R.contains(R.__, params.arr), 'id');
-			let propKey = params.key;
-			let propVal = params.value;
-			const updateParams = R.when(
-				itemInArr,
-				R.assoc(propKey, propVal),
-			);
-			params.obj = R.map(updateParams,params.obj);
-			params['action'] = this.paramActions[params.key];
-			return params;
-
-		};
-
-
-
-		const destroyItemFn = (params)  =>  {
-
-			params.obj = R.reject(R.propSatisfies(R.contains(R.__, params.arr), 'id'), params.obj)
-			params['action'] = this.paramActions[params.key];
-			return params;
-
-		};
-
-		const titleNewFn = (params) => {
-			const title = params.value;
-			let newItemObj = {
-				id: params.arr,
-				title,
-				completed: false
-			};
-
-			params.obj = R.append(newItemObj, params.obj);
-			params['action'] = this.paramActions[params.key];
-			return params;
-		};
-
-
-
-
-
-
-
-
-		const todoParams =  (key, value, arr, obj) => ({ key, value, arr, obj });
-
-		const completedAllParams = (p,o) => updateParamsInObj(todoParams('completed', getCompletedAllItemsBool(), completedAllArr(o), o));
-		const completedItemParams = (p,o) => updateParamsInObj(todoParams('completed', getCompletedItemBool(p), R.of(itemArr(p)), o));
-		const destroyItemParams = (p,o) => destroyItemFn(todoParams('destroy', undefined,  R.of(itemArr(p)), o));
-		const destroyAllParams = (p,o) => destroyItemFn(todoParams('destroy',undefined, destroyItemsArr(o), o));
-		const titleItemParams = (p,o) => updateParamsInObj(todoParams('title', mouseInputValueFn(p), R.of(itemArr(p)), o));
-		const titleNewParams = (p,o) => titleNewFn(todoParams('titleNew', mouseInputValueFn(p), this.getNextId(), o));
-
-
-
-
-
-		const fParamsList = {completedAllParams, completedItemParams, destroyItemParams, destroyAllParams, titleItemParams, titleNewParams};
-
-*/
-
-
 		this.ENTER_KEY = 13;
 
-		const camelCase = (str) => str.replace(/[-_]([a-z])/g, (m) => m[1].toUpperCase());
 
 		let isEnterKey = R.pathEq(['mouse','keyCode'], this.ENTER_KEY);
 		let inputIsNotEmpty =  R.pathSatisfies(R.complement(R.isEmpty), ['mouse','target','value']);
@@ -114,19 +36,13 @@
 
 		const filterUIElements = R.either(checkForBtnEvents, filterInputValue);
 
-		//const getCompletedAllItemsBool = () => this.completedItemsBool = !this.completedItemsBool;
-		const getCompletedAllItemsBool = () => document.querySelectorAll('.todo-list li.todos').length !== document.querySelectorAll('.todo-list li.completed').length;
+		const getCompletedAllItemsBool = ()=>!R.all(R.equals(true), R.map(x=>x.classList.contains('completed'))(document.querySelectorAll('.todo-list li')));
 		const getCompletedItemBool =  R.pathEq(['mouse','target','checked'], true);
 
 
 		// PULL ITEMS TO MUTATE
-		const completedAllArr = R.pluck('id');
-		const allListArr = () => document.querySelectorAll('.todo-list li.todos').map(li => li.dataset.id);
-		const destroyItemsArr = () => document.querySelectorAll('.todo-list li.todos.completed').map(li => li.dataset.id); // R.compose(R.pluck('id'), R.filter(R.propEq('completed', true)));
-		const itemArr = R.path(['data','id']);
-
-
-
+		const allListArr = () => R.map(x=>x.dataset.id,  document.querySelectorAll('.todo-list li.todos'));
+		const destroyItemsArr = () =>  R.map(x=>x.dataset.id,  document.querySelectorAll('.todo-list li.todos.completed'));
 
 		const updateFn = (key, val, list, obj) => {
 			const itemInList = R.propSatisfies(R.contains(R.__, list), 'id');
@@ -136,8 +52,6 @@
 
 		const destroyFn = (key, val, list, obj)  =>  R.reject(R.propSatisfies(R.contains(R.__, list), 'id'), obj);
 		const titleFn = (key, title, id, obj, completed = false) =>  R.append({id,title,completed}, obj);
-
-
 
 		const todoImpureData = (p,o) => {
 			const key = R.head(R.split('-', p.data.type));
@@ -183,8 +97,8 @@
 			const output = (args, o) => {
 				const {key, action, val, list, fn } = args;
 				const obj = fn(key,val,list,o);
-				const events = {key,list,val,action};
-				return {events, obj};
+				const payload = {key,list,val,action};
+				return {action, payload, obj};
 			};
 
 
@@ -215,15 +129,19 @@
 
 	getNextId(){
 		const padMaxNum = 6;
-		this.idIter = this.getHighestIdNum()+1;
-		const padNum = padMaxNum - String(this.idIter).length;
-		console.log('id is ',padNum,padMaxNum,String(this.idIter).length);
-		return String('0').repeat(padNum)+this.idIter;
-
+		const num = this.getHighestIdNum()+1;
+		const padNum = padMaxNum - String(num).length;
+		return String('0').repeat(padNum)+num;
 	}
 
+	 getHighestIdNum(){
+		 const getNum =  R.compose(R.defaultTo(0), parseInt, R.last, R.pluck('id'));
+		 return getNum(this.localStorageObj);
+	 }
 
 
+
+/*
 	addTodoObject(val){
 		let obj = {
 			id: this.getNextId(),
@@ -237,7 +155,7 @@
 		console.log("STORAGE ",typeof(this.localStorageObj), JSON.stringify(obj), obj);
 		this.setStorage(this.localStorageObj);
 		this.onSendStream('ADD_TODO_EVENT', obj);
-	}
+	}*/
 
 	onSendStream(a, o){
 		let action = this.actions[a];
@@ -247,10 +165,6 @@
 
 	getStorageItems(){
 		 return JSON.parse(localStorage.getItem(this.STORAGE_KEY)) || this.setStorage();
-	}
-	getHighestIdNum(){
-		const getNum =  R.compose(R.defaultTo(0), parseInt, R.last, R.pluck('id'));
-		return getNum(this.localStorageObj);
 	}
 
 	setStorage(obj=[]) {
